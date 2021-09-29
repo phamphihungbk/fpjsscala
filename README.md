f4c56aed94742c21eaf8953c64507d7331a6ad3f
Hung Pham <phamphihungbk@gmail.com>
2021-09-29 16:16:05 +0700

    update: sequence practicing

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-##🎃 Functional Programming Javascript
+## 🎃Functional Programming Javascript
+
 The repository to practice functional programming with Javascript.
+
+### FP Principles and Practice:
+
+- Immutability
+- Disciplined state
+- Pure functions and no side effects/disciplined states
+- First class functions and high order functions
+- Type systems
+- Referential transparency
+
+### Reference Documents:
+
+- https://subscription.packtpub.com/book/application-development/9781786461483/1/ch01lvl1sec8/principles-of-functional-programming#:~:text=FP%20is%20a%20way%20of,of%20constructing%20and%20building%20programs.&text=FP%20favors%20disciplined%20state%20management,Java%2C%20Python%2C%20and%20Ruby.
+- https://www.freecodecamp.org/news/the-principles-of-functional-programming/
--- sequence.js
+++ sequence.js
@@ -1,16 +1,63 @@
+/**
+ * Find the max number of array
+ */
 const numbers = [1, 7, 10, 9, 8];
 
+/**
+ * Solution 1
+ */
 numbers.sort(function (a, b) {
 	return a === b ? 0 : a > b ? -1 : 1;
 })
 
 console.log(numbers[0])
 
-
+/**
+ * Solution 2
+ */
 console.log(numbers.reduce(function (result, number) {
 	return result < number ? number : result;
 }));
 
+/**
+ * Reverse a string
+ */
 const string = 'robert';
 
+/**
+ * Solution 1
+ */
 console.log(string.split('').reverse().join(''));
+
+/**
+ * Solution 2
+ */
+console.log(string.split('').reduceRight(function (previous, current) {
+	return	previous + current;
+}));
+
+/**
+ * Capitalize First Letter of each word
+ */
+
+const text = "this is a simple text";
+
+/**
+ * Solution 1
+ */
+console.log(text.split(' ').map(function(word) {
+	return word.charAt(0).toUpperCase() + word.substring(1);
+}).join(' '));
+
+/**
+ * Solution 2
+ */
+function capitalizeWord(word){
+	return word.charAt(0).toUpperCase() + word.substring(1);
+}
+
+function capitalizeSentance(text, word){
+	return `${text} ${capitalizeWord(word)}`
+}
+
+console.log(text.split(' ').reduce(capitalizeSentance, '').trim());
